337f100b72677464e0ecebad8b6321cd63d62c19
ale-pc <bura.alessandro@gmail.com>
2025-08-18 12:48:36 +0200

    added script to run only twenties

---
+++ runner_twenties.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# initialize a semaphore with a given number of tokens
+open_sem(){
+    mkfifo pipe-$$
+    exec 3<>pipe-$$
+    rm pipe-$$
+    local i=$1
+    for((;i>0;i--));
+    do
+        printf %s 000 >&3
+    done
+}
+
+# run the given command asynchronously and pop/push tokens
+run_with_lock(){
+    local x
+    # this read waits until there is something to read
+    read -u 3 -n 3 x && ((0==x)) || exit $x
+    (
+     ( "$@"; )
+    # push the return code of the command to the semaphore
+    printf '%.3d' $? >&3
+    )&
+}
+
+env=aoi
+# path to the python interpreter in the conda environment
+pydir=/nfsd/signet4/burattoale/miniconda3/envs/$env/bin
+
+
+echo "Running binary exponential simulations"
+# number of parallel simulations
+n_paral=2
+open_sem $n_paral
+# main loop for starting simulations
+twenties=(
+    experiments/twenty.json
+    experiments/twenty_loc_aware.json
+)
+for i in "${twenties[@]}"; do
+    echo "Running $i"
+    run_with_lock $pydir/python runner.py --config $i
+done
